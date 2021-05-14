6923102344c83247a2c1ad88a2e92aded4167938
unknown <john.mejias@udea.edu.co>
2021-05-14 14:55:23 -0500

    primer commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import "bootstrap/dist/css/bootstrap.css"
+
+import Exercises from "./pages/Exercises"
+
+
+const container = document.getElementById('root')
+
+ReactDOM.render(<Exercises/>, container)
+
+
+
