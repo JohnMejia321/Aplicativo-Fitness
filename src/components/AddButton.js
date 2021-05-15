9fd2b6893d066567b7ee5b8d407f4edbc143395d
unknown <john.mejias@udea.edu.co>
2021-05-14 19:52:52 -0500

    Se agrego react router

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './styles/AddButton.css'
 import buttomImg from '../images/add.png'
+import { Link } from 'react-router-dom'
 
-
-const AddButton = () => (
-    
+const AddButton = (props) => (
+    <Link to="/exercises/new">
         <img src={buttomImg} className="Fitness-Add" alt="exercise"/>
- 
+    </Link>
 )
 
 export default AddButton
--- src/components/ExerciseList.js
+++ src/components/ExerciseList.js
@@ -1,27 +1,21 @@
-import React from "react"
-import Card from "./Card"
+import React from "react";
+import Card from "./Card";
 
-
-function ExerciseList(props){
-    return(
-        <div>
-              {props.exercises.map((exercise)=>{
-          return(
-            <Card
+const ExerciseList = ({exercises}) => (
+  <div>
+    {exercises.map((exercise) => {
+      return (
+        <Card
+          key={exercise.id}
           title={exercise.title}
           description={exercise.description}
           img={exercise.img}
           leftColor={exercise.leftColor}
           rightColor={exercise.rightColor}
         />
+      );
+    })}
+  </div>
+);
 
-          )
-        })
-        }
-        </div>
-    )
-
-}
-
-
-export default ExerciseList
+export default ExerciseList;
