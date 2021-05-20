30196f6dc27612e4f9674b2a2387439514d04abe
unknown <john.mejias@udea.edu.co>
2021-05-20 16:08:05 -0500

    Se agrega llamado al backend con fetch

--- src/pages/ExercisesNew.js
+++ src/pages/ExercisesNew.js
@@ -1,4 +1,6 @@
 import React from "react"
+import ExerciseForm from "../components/ExerciseForm"
+import Card from "../components/Card"
 
 
 
@@ -6,102 +8,45 @@ import React from "react"
 
 class ExercisesNew extends React.Component{
 
-    state= {}
 
 
-
-    handleSubmit=(e)=>{
-        e.preventDefault()
-        console.log(this.state)
+    state = {
+        form: {
+            title: '',
+            description: '',
+            img: '',
+            leftColor: '',
+            rightColor: ''
+        }
     }
 
-
-    handleChange=(e)=>{
+    handleChange = e => {
         this.setState({
-            [e.target.name]: e.target.value
+            form: {
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            }
         })
-    
-    
     }
-
 
     render(){
-        return(
-            <div className="container">
-            <form onSubmit={this.handleSubmit} >
-
-                <div className="form-group">
-                    <input 
-                        type="text" 
-                        className="form-control" 
-                        placeholder="title" 
-                        name="title"
+        return (
+            <div className="ExerciseNew_Lateral_Spaces row">
+                <div className="col-sm ExerciseNew_Card_Space">
+                    <Card 
+                        {...this.state.form}
+                    />
+                </div>
+                <div className="col-sm ExerciseNew_Form_Space">
+                    <ExerciseForm
                         onChange={this.handleChange}
-                        value={this.state.title}
-
+                        form={this.state.form}
                         
-                    />
+                    />            
                 </div>
-                <div className="form-group">
-                    <input 
-                        type="text" 
-                        className="form-control" 
-                        placeholder="description" 
-                        name="description"
-                        onChange={this.handleChange}
-                        value={this.state.description}
-                        
-                    />
-                </div>
-                <div className="form-group">
-                    <input 
-                        type="text" 
-                        className="form-control" 
-                        placeholder="img" 
-                        name="img"
-                        onChange={this.handleChange}
-                        value={this.state.img}
-                    
-                    />
-                </div>
-                <div className="form-row">
-                    <div className="col">
-                        <input 
-                            type="text" 
-                            className="form-control" 
-                            placeholder="leftColor" 
-                            name="leftColor"
-                            onChange={this.handleChange}
-                            value={this.state.leftColor}
-                           
-                        />
-                    </div>
-                    <div className="col">
-                        <input 
-                            type="text" 
-                            className="form-control"
-                            placeholder="rightColor" 
-                            name="rightColor"
-                            onChange={this.handleChange}
-                            value={this.state.rightColor}
-                           
-                        />    
-                    </div>
-                </div>
-                
-                <button 
-                    type="submit" 
-                    className="btn btn-primary"
-                >
-                    Submit
-                </button>
-            </form>
-        </div>
+            </div>
         )
-        
     }
 }
-
-
 
 export default ExercisesNew
